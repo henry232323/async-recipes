6e01238f1d250c099b0de4886017162d56828608
Henry <henry@typheus.me>
2017-12-15 21:20:36 -0700

    Add files via upload

--- async-recipes/filelikes.py
+++ async-recipes/filelikes.py
@@ -5,10 +5,22 @@ but on certain Linux-type systems stdin and stdout can be used with the
 default selector.
 Essentially uses a conditional Future to figure out when select is ready
 for a read/write, and waits until then.
+
+`AsyncFile.connect`
+This sets the socket to non-blocking, thus, using any recv method
+while waiting on a connect will make it instantly ready (select will
+assume ready to read) and then error. tl;dr Dont try to read before
+you've connected.
+
+What it does: waits until we can successfully get our peername. If
+it errors, that means we arent connected, and need to try another pass.
+If we get a different error (like failed to connect completely) it will
+raise.
 """
 
 import select
 from types import coroutine
+import errno
 
 from .future import Conditional
 
@@ -29,6 +41,14 @@ class AsyncFile:
         return self.file.read(nbytes)
 
     @coroutine
+    def readwith(self, func, *args, **kwargs):
+        """Read using a custom function, args will be forwarded. Will call the function when ready to read.
+        AsyncFile.file to pass the underlying socket"""
+        cond = Conditional(lambda: select.select([self.file], [], [], 0)[0])
+        yield from cond
+        return func(*args, **kwargs)
+
+    @coroutine
     def send(self, data):
         cond = Conditional(lambda: select.select([], [self.file], [], 0)[1])
         yield from cond
@@ -45,6 +65,28 @@ class AsyncFile:
         cond = Conditional(lambda: select.select([self.file], [], [], 0)[0])
         yield from cond
         return self.file.accept()
+
+    @coroutine
+    def connect(self, hostpair):
+        self.file.setblocking(False)
+        self.file.connect_ex(hostpair)
+        while True:
+            try:
+                self.file.getpeername()
+                break
+            except OSError as err:
+                if err.errno == errno.ENOTCONN:
+                    yield
+                else:
+                    raise
+
+        self.file.setblocking(True)
+
+    def dup(self):
+        return AsyncFile(self.file.dup())
+
+    def makefile(self, mode='r', buffering=None, *, encoding=None, errors=None, newline=None):
+        return AsyncFile(self.file.makefile(mode, buffering, encoding=encoding, errors=errors, newline=newline))
 
     def fileno(self):
         return self.file.fileno()
